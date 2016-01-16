2c7fedec61a667e5665b7e6dae8d92692b6d3275
initcron-abhijit <abhijit.chaudhari@initcron.org>
2016-01-16 15:06:38 +0530

    Removing extra code

--- attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default[:asql][:root_pass] = 'change'
-default[:asql][:user_list] = [{ :username => 'wordpress', :password => 'pass'}]
-default[:asql][:databases] = [{:name => 'wordpress'}]
-default[:asql][:hostname] = 'localhost'
-default['asql']['server_name'] = 'localhost'
-default['asql']['dbname'] = 'wordpress'
-default['asql']['dbusername'] = 'wordpress'
-default['asql']['dbpassword'] = 'pass'
-
